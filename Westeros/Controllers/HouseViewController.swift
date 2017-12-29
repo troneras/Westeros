1273a28b956b4c3be53a0241140c70fad3bd756c
Alexandre <xandrefreire@gmail.com>
2017-12-29 13:35:26 +0100

    Creation of HouseViewController and outlets. Explanation of Life Cycle, Delegate, init(nibName:bubdle:)

---
+++ Westeros/Controllers/HouseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HouseViewController.swift
+//  Westeros
+//
+//  Created by Alexandre Freire García on 29/12/17.
+//  Copyright © 2017 Westeros. All rights reserved.
+//
+
+import UIKit
+
+class HouseViewController: UIViewController {
+
+    @IBOutlet weak var houseNameLabel: UILabel!
+    
+    @IBOutlet weak var wordsLabel: UILabel!
+    
+    @IBOutlet weak var sigilImageView: UIImageView!
+    
+    
+    
+}
