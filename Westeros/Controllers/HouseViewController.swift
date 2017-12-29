5a525d3803f732d06150ccae9ef731d44e987412
Alexandre <xandrefreire@gmail.com>
2017-12-29 13:42:18 +0100

    House View Controller with model

--- Westeros/Controllers/HouseViewController.swift
+++ Westeros/Controllers/HouseViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class HouseViewController: UIViewController {
 
+    // MARK: - Outlets
     @IBOutlet weak var houseNameLabel: UILabel!
     
     @IBOutlet weak var wordsLabel: UILabel!
     
     @IBOutlet weak var sigilImageView: UIImageView!
     
+    // MARK: - Properties
+    let model: House
     
+    // MARK: - Initialization
+    init(model: House) {
+        self.model = model
+        super.init(nibName: nil, bundle: nil)
+    }
     
+    // Chaputa de los de Cupertino relacionada con los nil
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        syncViewModel()
+    }
+    
+    // MARK: - Sync
+    func syncViewModel() {
+        // Model -> View
+        houseNameLabel.text = "Hoyse \(model.name)"
+        sigilImageView.image = model.sigil.image
+        wordsLabel.text = model.words
+    }
 }
