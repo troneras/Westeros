5ce2cd443d877d45b8693a0292613f31bdf00db1
Alexandre Freire <xandrefreire@gmail.com>
2017-12-31 14:04:47 +0100

    Add loadingView to WikiViewController (delegates)

--- Westeros/Controllers/WikiViewController.swift
+++ Westeros/Controllers/WikiViewController.swift
@@ -13,6 +13,7 @@ class WikiViewController: UIViewController {
     
     // Mark: - Outlets
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loadingView: UIActivityIndicatorView!
     
     // Mark: - Properties
     let model: House
@@ -30,13 +31,35 @@ class WikiViewController: UIViewController {
     // Mark: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
         syncViewWithModel()
+        
     }
     
     // Mark: - Sync
     func syncViewWithModel() {
+        loadingView.isHidden = false
+        loadingView.startAnimating()
         title = model.name
         webView.load(URLRequest(url: model.wikiURL))
+        
+    }
+}
+
+extension WikiViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loadingView.stopAnimating()
+        loadingView.isHidden = true
     }
     
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        let type = navigationAction.navigationType
+        switch type {
+        case .linkActivated, .formSubmitted:
+            decisionHandler(.cancel)
+        default:
+            decisionHandler(.allow)
+        }
+    }
 }
