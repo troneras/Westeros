dcdafe685892a0e689bd9708ba72b123faa038e8
Alexandre Freire <xandrefreire@gmail.com>
2018-02-02 18:35:44 +0000

    Add SeasonListVC and SeasonDetailVC

---
+++ Westeros/Controllers/SeasonDetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SeasonDetailViewController.swift
+//  Westeros
+//
+//  Created by Alexandre Freire on 02/02/2018.
+//  Copyright © 2018 Westeros. All rights reserved.
+//
+
+import UIKit
+
+class SeasonDetailViewController: UIViewController {
+
+    // Mark: - Outlets
+    @IBOutlet weak var nameField: UILabel!
+    @IBOutlet weak var airDateField: UILabel!
+    @IBOutlet weak var episodesCountField: UILabel!
+    
+    // Mark: - Properties
+    let model: Season
+    
+    // Mark: - Initialization
+    init(model: Season) {
+        self.model = model
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Mark: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        syncModelWithView()
+    }
+
+   
+    // Mark: - Sync
+    func syncModelWithView() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        
+        title = "Season"
+        nameField.text = model.name
+        let date = dateFormatter.string(from: model.releaseDate)
+        airDateField.text = "Air date: \(date)"
+        episodesCountField.text = "No. of episodes: \(model.count)"
+    }
+
+    // Mark: - Actions
+    @IBAction func episodesDidTap(_ sender: Any) {
+        fatalError()
+    }
+    
+}
---
+++ Westeros/Controllers/SeasonListViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SeasonListViewController.swift
+//  Westeros
+//
+//  Created by Alexandre Freire on 02/02/2018.
+//  Copyright © 2018 Westeros. All rights reserved.
+//
+
+import UIKit
+
+class SeasonListViewController: UITableViewController {
+    
+    // Mark: - Properties
+    let model: [Season]
+    
+    // Mark: - Initialization
+    init(model: [Season]) {
+        self.model = model
+        super.init(style: .plain)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+ 
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellID = "SeasonCell"
+        // Descubrir cual es la temporada que tenemos que mostrar
+        let season = model[indexPath.row]
+        
+        // Crear una celda
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellID)
+        }
+        
+        // Sincronizar person -> cell
+        cell?.textLabel?.text = season.name
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        let date = dateFormatter.string(from: season.releaseDate)
+        cell?.detailTextLabel?.text = "Air date: \(date)"
+        
+        return cell!
+    }
+    
+    // Mark: - Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let season = model[indexPath.row]
+        
+        let viewController = SeasonDetailViewController(model: season)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
